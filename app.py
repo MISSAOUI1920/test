57cd136da37d2d2f3b1a91502195f176938c7d24
M12 <121401817+MISSAOUI1920@users.noreply.github.com>
2024-07-20 17:37:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import os
+os.system('pip install huggingface_hub==0.15.1')
 from huggingface_hub import InferenceClient
+
 client = InferenceClient(
         "meta-llama/Meta-Llama-3-8B-Instruct",
         token="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",
